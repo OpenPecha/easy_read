91ddd679b9d874758432ef6c15f76a164fbdcf6e
Tenzin Kalden <tenkalden007@gmail.com>
2024-01-30 13:53:37 +0530

    minor changes

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 def get_tokens(wt, text):
     tokens = wt.tokenize(text, split_affixes=False)
-    return tokens
+    return '  '.join(token['text'] for token in tokens)
 
 @app.route('/')
 def index():
