95ab74a9e3212f0d6622617f5e870e5febf06dfc
Tenzin Kalden <tenkalden007@gmail.com>
2024-02-05 15:31:46 +0530

    minor change

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
-from flask import render_template, request, send_file
+from flask import Flask, render_template, request, send_file
 from botok import WordTokenizer
 from botok.config import Config
 from pathlib import Path
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 def get_tokens(wt, text):
     tokens = wt.tokenize(text, split_affixes=False)
@@ -14,27 +13,22 @@ def get_tokens(wt, text):
 def index():
     return render_template('index.html')
 
-@app.route('/tokenize', methods=['post'])
+@app.route('/tokenize', methods=['POST'])
 def tokenize():
     config = Config(dialect_name='general', base_path=Path.home())
-    wt= WordTokenizer(config=config)
+    wt = WordTokenizer(config=config)
     input_text = request.form['ipText']
-    tokens = get_tokens(wt, input_text,)
-    ouput_text = "".join([str(token) for token in tokens])
+    tokens = get_tokens(wt, input_text)
+    output_text = "".join([str(token) for token in tokens])
 
-    return render_template('index.html',input_text=input_text,output = ouput_text)
+    return render_template('index.html', input_text=input_text, output=output_text)
 
-@app.route('/download_token',methods=['post'])
+@app.route('/download_token', methods=['POST'])
 def download_token():
     output_text = request.form['opText']
     with open('output.txt', 'w', encoding='utf-8') as file:
-              file.write(output_text) 
+        file.write(output_text)
     return send_file('output.txt', as_attachment=True)
-
-
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
